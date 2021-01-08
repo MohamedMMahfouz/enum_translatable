5f74e019f0df169852a47d539eaccb029a9a78a2
mohamedmmahfouz <mohamed.abdel-moneam@gmail.com>
2021-01-08 20:24:22 +0200

    accommodate all I18n locales

--- lib/enum_translatable.rb
+++ lib/enum_translatable.rb
@@ -9,18 +9,14 @@ module EnumTranslatable
       define_method :"translated_#{key}" do
         enum_value = send(key)
         return nil unless enum_value.present?
-        I18n.t("activerecord.attributes.#{model_name.singular}.#{key}_list.#{enum_value}", default: enum_value)
+        I18n.t("activerecord.enum_translatable.#{model_name.singular}.#{key}.#{enum_value}", default: enum_value)
       end
 
-      define_method :"#{key}_en" do
-        I18n.with_locale(:en) do
-          send("translated_#{key}")
-        end
-      end
-
-      define_method :"#{key}_ar" do
-        I18n.with_locale(:ar) do
-          send("translated_#{key}")
+      I18n.available_locales.each do |locale|
+        define_method :"#{key}_#{locale}" do
+          I18n.with_locale(locale) do
+            send("translated_#{key}")
+          end
         end
       end
     end
