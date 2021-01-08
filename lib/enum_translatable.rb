8b681282f0c88f17563be6ed62dc0e1b29ec924b
mohamedmmahfouz <mohamed.abdel-moneam@gmail.com>
2021-01-08 20:08:43 +0200

    add concerns

--- lib/enum_translatable.rb
+++ lib/enum_translatable.rb
@@ -1,6 +1,28 @@
 require "enum_translatable/version"
+require 'active_support/concern'
 
 module EnumTranslatable
-  class Error < StandardError; end
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  included do
+    defined_enums.keys.each do |key|
+      define_method :"translated_#{key}" do
+        enum_value = send(key)
+        return nil unless enum_value.present?
+        I18n.t("activerecord.attributes.#{model_name.singular}.#{key}_list.#{enum_value}", default: enum_value)
+      end
+
+      define_method :"#{key}_en" do
+        I18n.with_locale(:en) do
+          send("translated_#{key}")
+        end
+      end
+
+      define_method :"#{key}_ar" do
+        I18n.with_locale(:ar) do
+          send("translated_#{key}")
+        end
+      end
+    end
+  end
 end
--- spec/enum_translatable_spec.rb
+++ spec/enum_translatable_spec.rb
@@ -1,9 +1,2 @@
 RSpec.describe EnumTranslatable do
-  it "has a version number" do
-    expect(EnumTranslatable::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
